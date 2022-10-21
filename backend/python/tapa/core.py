91b9c609f441912ff76468c450f613c636150a9a
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-21 23:57:07 +0000

    Adding tarfile member sanitization to extractall()

--- backend/python/tapa/core.py
+++ backend/python/tapa/core.py
@@ -248,7 +248,26 @@ class Program:
     _logger.info('extracting RTL files')
     for task in self._tasks.values():
       with tarfile.open(self.get_tar(task.name), 'r') as tarfileobj:
-        tarfileobj.extractall(path=self.work_dir)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tarfileobj, path=self.work_dir)
 
     for file_name in (
         'arbiter.v',
